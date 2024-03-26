ff49d578629d0c029323affcb85fd9d916c8b6d3
Prithvi-Pratap <pratapprithvi768@gmail.com>
2024-03-27 01:22:32 +0530

    useRef assignment

--- week-6/3-use-ref/src/components/Assignment1.jsx
+++ week-6/3-use-ref/src/components/Assignment1.jsx
@@ -1,21 +1,32 @@
+import { useRef } from "react";
 import { useEffect } from "react";
 
 // Create a component with a text input field and a button. When the component mounts or the button is clicked, automatically focus the text input field using useRef.
 
 export function Assignment1() {
 
+    const textInput = useRef();
+
     useEffect(() => {
-
+        textInput.current.focus();
+        
     }, []);
-
+    
+    //focus and select the text input
     const handleButtonClick = () => {
+        textInput.current.focus();
+        textInput.current.select();
 
     };
 
     return (
         <div>
-            <input type="text" placeholder="Enter text here" />
+            <input ref={textInput} type="text" placeholder="Enter text here" />
             <button onClick={handleButtonClick}>Focus Input</button>
         </div>
     );
 };
+
+//few take aways from this assignment :
+//1.the useRef() can be used to store a reference to dom elements
+//2.the useRef() can be also be used to store numbers or whatever you want to store , and it only renders once the component mount , it doesn't change its value to intial value in every render
--- week-6/3-use-ref/src/components/Assignment2.jsx
+++ week-6/3-use-ref/src/components/Assignment2.jsx
@@ -1,19 +1,30 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback,useRef } from 'react';
 
 // Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
 
 export function Assignment2() {
-    const [, forceRender] = useState(0);
+    const [count,setCount] = useState(0);
 
-    const handleReRender = () => {
-        // Update state to force re-render
-        forceRender(Math.random());
-    };
+    const numberOfRendered=useRef(0);// useRef creates a mutable object with current property,  it means that useRef allows you to hold onto values that may change over time (mutable values) without triggering unnecessary re-renders of your component.
+    
+    console.log("re-render");
+
+    const handleReRender = useCallback(() => {
+        setCount(prev=>prev+1);
+    },[]);
+
+    numberOfRendered.current=numberOfRendered.current + 1;
 
     return (
         <div>
-            <p>This component has rendered {0} times.</p>
+            <p>This component has rendered {numberOfRendered.current} times.</p>
             <button onClick={handleReRender}>Force Re-render</button>
         </div>
     );
 };
+
+
+//few take aways from this assignment :
+//1.the useRef() can be used to store a reference to dom elements
+//2.the useRef() can be also be used to store numbers or whatever you want to store , and it only renders once the component mount , it doesn't change its value to intial value in every render . it persists till the lifetime of the component.
+//3. useRef() returns a mutable object with the 'current' property.
